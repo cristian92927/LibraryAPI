a2795f644c01417737ab30457116918cd44b34f3
cristian92927 <ct92927@gmail.com>
2025-10-03 15:22:32 -0500

    Implementación inicial de modelo, servicio, controlador y configuración para gestión de libros con SQL Server

---
+++ Data/LibraryContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
